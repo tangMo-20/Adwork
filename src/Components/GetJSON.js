6922b5bf7da199916f374425d94e4cf6625ee38f
oleksandr.khodos <oleksandr.khodos@akvelon.com>
2018-02-05 20:42:44 +0200

    front-end

--- src/Components/GetJSON.js
+++ src/Components/GetJSON.js
@@ -1,5 +1,3 @@
-//import url_config from "./url_config";
-
 export const GetTopFive = () => {
     return fetch('http://localhost:8080/get_top_five', {
         method: "GET",
@@ -27,36 +25,12 @@ export const GetInStock = () => {
 };
 
 export const PostSearch = (searchString) => {
-    //debugger;
-    //let stringif = JSON.stringify(searchRequest);
     return fetch('http://localhost:8080/search/' + searchString,{
             method: "GET",
             mode: "cors",
-            // headers: {
-            //     "Content-Type": "application/json",
-            //     "Access-Control-Allow-Origin": "*",
-            // },
-            // body: {
-            //     "searchString": searchString
-            // },
         })
         .then(function(response){
             //console.log(response.json());
             return response.json();
         })
 };
-
-// export const GetBySearch = (searchRequest) => {
-//     return fetch('http://localhost:8080/search/${searchRequest}', {
-//         method: "GET",
-//         mode: "cors",
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Access-Control-Allow-Origin": "*",
-//         }
-//     })
-//         .then(function(response) {
-//             console.log(response.json());
-//             //return response.json();
-//         });
-// };
--- src/Components/url_config.js
+++
@@ -1,5 +0,0 @@
-const MAIN_SERVER  = 'http://192.168.2.194:8080';
-
-export default {
-    url: `${ MAIN_SERVER  }`,
-}
